7a3392943c3ab82c16717b96eb4129f7839ba417
Norman Chen <bird.dream.norman@gmail.com>
2024-08-23 15:16:46 +0800

    crowd funding homework

--- l1-5-homework/crowdFunding.sol
+++ l1-5-homework/crowdFunding.sol
@@ -2,5 +2,58 @@
 pragma solidity ^0.8.17;
 
 contract CrowdFunding {
-    
+    address public immutable beneficiary;
+    uint256 public immutable fundingGoal;
+    uint256 public fundingAmount; // current money
+    mapping(address => uint256) public funders;
+    mapping(address => bool) private funderInserted;
+    address[] public fundersKey;
+    bool public AVAILABLED = true; //
+
+    constructor(address beneficiary_, uint256 goal_) {
+        beneficiary = beneficiary_;
+        fundingGoal = goal_;
+    }
+
+    function contribute() external payable {
+        require(AVAILABLED, "CrowdFunding is closed");
+
+        uint256 potentialFundingAmount = fundingAmount + msg.value;
+        uint256 refundAmount = 0;
+
+        if(potentialFundingAmount > fundingGoal) {
+            refundAmount = potentialFundingAmount - fundingGoal;
+            funders[msg.sender] += (msg.value - refundAmount);
+            fundingAmount += (msg.value - refundAmount);
+        } else {
+            funders[msg.sender] += msg.value;
+            fundingAmount += msg.value;
+        }
+
+        if(!funderInserted[msg.sender]) {
+            funderInserted[msg.sender] = true;
+            fundersKey.push(msg.sender);
+        }
+
+        if(refundAmount > 0) {
+            payable(msg.sender).transfer(refundAmount);
+        }
+    }
+
+    function close() external returns(bool) {
+        if(fundingAmount < fundingGoal) {
+            return false;
+        }
+        uint256 amount = fundingAmount;
+
+        fundingAmount = 0;
+        AVAILABLED = false;
+
+        payable(beneficiary).transfer(amount);
+        return true;
+    }
+
+    function funderLength() public view returns(uint256) {
+        return fundersKey.length;
+    }
 }
